75e297d6fcc400cc16c01d23eeb7e851768f796e
Kavinsha Chamod <kavinshachamod2001@gmail.com>
2025-09-28 20:44:11 +0530

    add endpoint to retrieve all latest bus locations

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -21,3 +21,22 @@ export const getLatestLocation = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
+
+export const getAllLatestLocations = async (req, res) => {
+  try {
+    const buses = await Location.aggregate([
+      {
+        $sort: { timestamp: -1 }
+      },
+      {
+        $group: {
+          _id: "$bus_id",
+          latestLocation: { $first: "$$ROOT" }
+        }
+      }
+    ]);
+    res.json(buses);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
--- routes/locationRoutes.js
+++ routes/locationRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { addLocation, getLatestLocation } from "../controllers/locationController.js";
+import { addLocation, getLatestLocation, getAllLatestLocations } from "../controllers/locationController.js";
 
 const router = express.Router();
 
 router.post("/", addLocation);
 router.get("/:busId", getLatestLocation);
+router.get("/", getAllLatestLocations);
 
 export default router;
